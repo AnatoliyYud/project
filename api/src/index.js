33e189472ad25d51e3b9e465b8ada85db85d0ff8
AnatoliyYud <trouble93@yandex.ru>
2022-08-31 12:29:54 +0000

    modify api/src/index.js

--- api/src/index.js
+++ api/src/index.js
@@ -4,21 +4,28 @@ const { port, host, db } = require("./configuration");
 const { connectDb } = require("./helpers/db");
 
 const app = express();
-const postSchema = new mongoose.Schema({
+const kittySchema = new mongoose.Schema({
   name: String
-const Post = mongoose.model("Post", postSchema);
+});
+const Kitten = mongoose.model("Kitten", kittySchema);
+
+app.get("/test", (req, res) => {
+  res.send("Our api server is working correctly");
+});
 
 const startServer = () => {
   app.listen(port, () => {
     console.log(`Started api service on port ${port}`);
-      console.log(`Our host is ${host}`);
-      console.log(`Our database ${db}`)
+    console.log(`Our host is ${host}`);
+    console.log(`Database url ${db}`);
+
+    const silence = new Kitten({ name: "Silence" });
+    silence.save(function(err, result) {
+      if (err) return console.error(err);
+      console.log("result", result);
+    });
   });
 };
-
-app.get("/test", (req, res) => {
-    res.send("Our api server is working correctly");
-});
 
 connectDb()
   .on("error", console.log)
